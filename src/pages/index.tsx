e07354175fef209e4c702820e04903f0f8436fa0
Gabriel g3 <gabrielfranccelino@gmail.com>
2021-04-04 19:14:18 -0300

    feat: global scss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
-import styles from "styles/home.module.scss";
+// import styles from "styles/home.module.scss";
+import Head from "next/head";
 
 function Home() {
-  return <div><h1 className={styles.title}>Hello NextJs</h1></div>;
+  return (
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+
+      <h1 >Hello NextJs</h1>
+    </>
+  );
 }
 
 export default Home;

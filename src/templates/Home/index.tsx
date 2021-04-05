5b11aa27ba7d345902c44c6493207a90523b8140
Gabriel g3 <gabrielfranccelino@gmail.com>
2021-04-05 13:38:09 -0300

    feat: component subscribe-button

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,6 +1,7 @@
 import styles from "./styles.module.scss";
 
 import Head from "next/head";
+import { SubscribeButton } from "components/SubscribeButton";
 
 export function HomeTemplate() {
   return (
@@ -21,6 +22,8 @@ export function HomeTemplate() {
             Get access to all the publication <br />
             <span>for $9.90 month</span>
           </p>
+
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />

983364ce941d123a0117a2175a5dab0d15d48f4e
Gabriel g3 <gabrielfranccelino@gmail.com>
2021-04-07 20:55:00 -0300

    refactor: duplication stripe faunadb

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,18 +1,46 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import { query as q } from 'faunadb'
 import { getSession } from 'next-auth/client'
+import { fauna } from 'services/fauna'
 import { stripe } from 'services/stripe'
 
-export default async (req: NextApiRequest, response: NextApiResponse) => {
+type User = {
+  ref: {
+    id: string
+  }
+
+  data: {
+    stripe_customer_id: string
+  }
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     const session = await getSession({ req })
 
-    const stripeCustomer = await stripe.customers.create({
-      email: session.user.email
-      //   metadata
-    })
+    const user = await fauna.query<User>(
+      q.Get(q.Match(q.Index('user_by_name'), q.Casefold(session.user.email)))
+    )
+
+    let customerId = user.data.stripe_customer_id
+
+    if (!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email
+        //   metadata
+      })
+
+      await fauna.query(
+        q.Update(q.Ref(q.Casefold('users'), user.ref.id), {
+          data: { stripe_customer_id: stripeCustomer.id }
+        })
+      )
+
+      customerId = stripeCustomer.id
+    }
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCustomer.id,
+      customer: customerId,
       payment_method_types: ['card'],
       billing_address_collection: 'required',
       line_items: [
@@ -27,9 +55,9 @@ export default async (req: NextApiRequest, response: NextApiResponse) => {
       cancel_url: process.env.STRIPE_CANCEL_URL
     })
 
-    return response.status(200).json({ sessionId: stripeCheckoutSession.id })
+    return res.status(200).json({ sessionId: stripeCheckoutSession.id })
   } else {
-    response.setHeader('Allow', 'POST')
-    response.status(405).end('Method not allowed')
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method not allowed')
   }
 }
